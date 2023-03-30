1cbdabe2c21fc15e9451313f7e63a446deaf322f
Agustin Perez Pignalberi <agustinperezpignalberi@gmail.com>
2023-03-30 15:24:05 -0300

    port

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ app.use((request, response) => {
    });
 });
 
-const PORT = process.env.CARTS_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
 });
